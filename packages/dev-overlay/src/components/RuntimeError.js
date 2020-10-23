082a57562d524230c58a2b141e05a9928095248c
Lucian Cooper <cooperlucian@gmail.com>
2020-10-23 11:14:34 -0400

    feat(overlay): clean up runtime error stack trace display

--- packages/dev-overlay/src/components/RuntimeError.js
+++ packages/dev-overlay/src/components/RuntimeError.js
@@ -4,22 +4,26 @@ import './RuntimeError.scss';
 function StackFrame({ fn, compiled, src }) {
     const { file, loc: [line, col], ctx } = src || compiled;
     // determine location string
-    let loc;
+    let loc,
+        code;
     if (file && typeof line === 'number') {
-        loc = file
-            .replace(/^[/|\\].*?[/|\\]((?:src|node_modules)[/|\\].*)/, '$1')
-            .replace('webpack://', '.');
-        loc += `:${line}`;
+        loc = `${file.replace(/^webpack:\/{3}/, '')}:${line}`;
         if (col) loc += `:${col}`;
     } else {
         loc = 'unknown';
+    }
+    // determine if file location is internal
+    const isInternal = !file || /\/(?:~|node_modules)\//.test(file) || file.trim().indexOf(' ') !== -1;
+    // create code block if context exists and location is not an internal file
+    if (ctx && !isInternal) {
+        code = <CodeBlock line={line} {...ctx}/>;
     }
     // return stack frame element
     return (
         <div className='stack-frame'>
             <div className='function'>{fn || '(anonymous function)'}</div>
             <div className='location'>{loc}</div>
-            {ctx && <CodeBlock line={line} {...ctx}/>}
+            {code}
         </div>
     );
 }
